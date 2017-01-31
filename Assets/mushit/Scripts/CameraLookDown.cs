f1d511dd6d8259c72da3c3f5047d8a453f8b4079
Maiko Steeman <Maiko Steeman>
2017-01-31 12:01:05 +0100

    added falling camera

---
+++ Assets/mushit/Scripts/CameraLookDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookDown : MonoBehaviour {
+    public new Camera camera = null;
+    public bool lookDown = false;
+
+	void Start () {
+        if(camera == null)
+            camera = GetComponent<Camera>();
+	}
+	
+	void Update () {
+        if (!lookDown)
+            return;
+
+        //Quaternion current = gameObject.transform.rotation;
+        //Quaternion target = Vector3.down
+	}
+}
--- Assets/mushit/Scripts/GameManager.cs
+++ Assets/mushit/Scripts/GameManager.cs
@@ -7,19 +7,32 @@ public class GameManager : MonoBehaviour {
     public float maxHeight = 8;
     public bool inLava = false;
     private GameObject[] lavas;
+    private GameObject[] foots;
+    private GameObject mainCamera;
 
 	// Use this for initialization
 	void Start () {
         lavas = GameObject.FindGameObjectsWithTag("lava");
+        foots = GameObject.FindGameObjectsWithTag("foot");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!inLava)
-            return;
+        if (inLava) {
+            //MoveLavaUp();
+            FallDown();
+        }
+    }
 
+    void FallDown() {
+        mainCamera.GetComponent<Rigidbody>().isKinematic = false;
+        mainCamera.transform.SetParent(null);
+    }
+
+    void MoveLavaUp() {
         foreach (GameObject lava in lavas) {
-            if(lava.transform.position.y < maxHeight)
+            if (lava.transform.position.y < maxHeight)
                 lava.transform.position += Vector3.up * speed * Time.deltaTime;
         }
     }
